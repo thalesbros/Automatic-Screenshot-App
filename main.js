1886d7fff5b534f415b05a5811e2d5982af81a09
Thales Broussard <thalesbroussard@gmail.com>
2025-04-05 02:01:25 -0300

    Uploading icons and final updates
    Uploading icons and final updates

--- main.js
+++ main.js
@@ -164,6 +164,25 @@ ipcMain.on('open-save-folder', (event, directory) => {
     }
 });
 
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    console.log('Update available:', info);
+});
+autoUpdater.on('update-not-available', (info) => {
+    console.log('Update not available:', info);
+});
+autoUpdater.on('error', (err) => {
+    console.error('Error in auto-updater:', err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    console.log('Download progress:', progressObj);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    console.log('Update downloaded:', info);
+});
+
 app.on('ready', () => {
     createWindow();
     createTray();
